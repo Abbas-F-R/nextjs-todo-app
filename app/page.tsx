f585025d9f154ab0c0d3edc054f270ee61226c2f
Abbas-F-R <na308848@gmail.com>
2024-08-16 12:00:22 +0300

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
+import { useRef, useState } from "react";
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+type Todo = {
+    completed: boolean;
+    text: string;
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+function Home() {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    function addTodo() {
+        if (inputRef.current) {
+            const text: string = inputRef.current.value;
+            const newItem = { completed: false, text };
+            setTodos([...todos, newItem]);
+            inputRef.current.value = "";
+        }
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+    function handleItemDon(index: number) {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    function handleItemDelete(index: number) {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    }
+
+    return (
+        <main className="bg-gray-800 flex items-center justify-center min-h-screen">
+            <div className="w-full max-w-md sm:max-w-lg lg:max-w-xl xl:max-w-2xl p-4 bg-black rounded-lg shadow-lg outline outline-offset-2 outline-red-900 hover:shadow-2xl hover:outline-red-600 hover:shadow-red-500 transition-all duration-700">
+                <h1 className="text-3xl sm:text-4xl lg:text-5xl text-center text-white mb-6">To Do List</h1>
+                <div className="flex space-x-2">
+                    <input
+                        className="flex-1 outline-none border-none bg-red-800 rounded-lg p-2 text-white"
+                        placeholder='Add todo... '
+                        
+                        ref={inputRef}
+                        type="text"
+                    />
+                    <button
+                        className="bg-red-800 rounded-lg outline-none px-3 py-2 hover:bg-red-700 hover:shadow-lg hover:shadow-red-900 transition-all duration-300"
+                        onClick={addTodo}
+                    >
+                        Add
+                    </button>
+                </div>
+                <div className="flex flex-col space-y-3 pt-2">
+                    <ul className="space-y-2">
+                        {todos.map(({ text, completed }, index) => (
+                            <div className="flex justify-between items-center" key={index}>
+                                <li
+                                 className={`flex text-1xl px-4 mt-1 break-words truncate max-w-full cursor-pointer border-collapse hover:px-3 transition-all duration-150 ${completed ? "line-through" : ""}`}
+                                 onClick={() => handleItemDon(index)}
+                                >
+                                    {text}
+                                </li>
+                                <span>
+                                    <i
+                                        className='fa-solid fa-x mx-2 hover:text-red-600 transition-all duration-200 cursor-pointer'
+                                        onClick={() => handleItemDelete(index)}
+                                    ></i>
+                                </span>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </main>
+    );
 }
+
+export default Home;
